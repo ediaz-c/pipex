28d2e03a131cdc968864e7a0e0fb5d6a7fde9ce9
Erick <centeno222@live.com>
2023-05-30 16:26:47 +0200

    heredoc y proceso hijo quitado

--- bonus_utils/check_files_bonus.c
+++ bonus_utils/check_files_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_files_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ediaz--c <ediaz--c@student.42madrid>       +#+  +:+       +#+        */
+/*   By: ediaz--c <ediaz--c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 19:34:22 by ediaz--c          #+#    #+#             */
-/*   Updated: 2023/05/30 13:08:28 by ediaz--c         ###   ########.fr       */
+/*   Updated: 2023/05/30 13:53:55 by ediaz--c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,22 @@
 static void	ft_create_heredoc(char	*limiter)
 {
 	int		fd[2];
-	pid_t	pid;
 	char	*line;
 
 	open_pipe(fd);
-	pid = fork();
-	if (pid == 0)
+	while (1)
 	{
-		close(fd[0]);
-		while (1)
-		{
-			write(0, "heredoc> ", 9);
-			line = get_next_line(0);
-			if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
-				exit(0);
-			write(fd[1], line, ft_strlen(line));
-			free(line);
-		}
+		write(0, "heredoc> ", 9);
+		line = get_next_line(0);
+		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0
+			&& ft_strlen(line) - 1 == ft_strlen(limiter))
+			break ;
+		write(fd[1], line, ft_strlen(line));
+		free(line);
 	}
-	else
-	{
-		close(fd[1]);
-		dup2(fd[0], 0);
-		wait(NULL);
-	}
+	close(fd[1]);
+	dup2(fd[0], 0);
+	close(fd[0]);
 }
 
 void	ft_check_files(t_pipex	*pipex, char **args, int n_args)
